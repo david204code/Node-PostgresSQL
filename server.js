207d5385bc4f86c3f0f954066cdd0286f991a259
David Wu <34573725+david204code@users.noreply.github.com>
2022-06-01 18:09:51 +0100

    Added routes for the app to listen to, for a response and add an health check

--- server.js
+++ server.js
@@ -2,5 +2,15 @@ const express = require('express')
 const app = express();
 const port = 3000;
 
+//give it something to listen to on the / route with a callback function
+app.get("/", (req, res) => {
+    res.send("Hello and welcome!");
+});
+
+//check the health
+app.get("/health", (req, res) => {
+    res.status(200).json({ status: "OK", timestamp: new Date() })
+})
+
 //listen to the port and a callback function 
 app.listen(port, () => console.log(`This APP is listening on port ${port}`));
